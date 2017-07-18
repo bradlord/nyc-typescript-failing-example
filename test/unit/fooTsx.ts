89a55377cbd3d201e9fd4b156160f0a749be69c5
Bradley Lord <blord@mail.acponline.org>
2017-07-18 14:31:44 -0400

    Initial commit

---
+++ test/unit/fooTsx.ts
@@ -0,0 +1,14 @@
+import {expect} from "chai";
+import {Foo, makeFoo, incrementFoo} from "../../src/fooTsx";
+
+describe("makeFoo", function () {
+    it("handles negative numbers correctly", function () {
+        const foo = makeFoo(-5);
+        expect(foo.x).to.equal(5);
+    });
+
+    it("makes a Foo", function () {
+        // const foo = makeFoo(5);
+        // expect(foo.x).to.equal(5);
+    });
+});
